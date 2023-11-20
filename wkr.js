d957a19b73dd3102949696b5fc4f121ac83bfa74
pemersatudotfun <128120945+pemersatudotfun@users.noreply.github.com>
2023-11-20 17:30:00 +0700

    Update wkr.js

--- wkr.js
+++ wkr.js
@@ -1,32 +1,3 @@
-// Di bagian atas file Anda
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js');
-
-firebase.initializeApp({
-     apiKey: "AIzaSyADFzYFDM3D6__WXHZ0RvwxzHspzFvEwmw",
-        authDomain: "pemersatu-7b002.firebaseapp.com",
-        projectId: "pemersatu-7b002",
-        storageBucket: "pemersatu-7b002.appspot.com",
-        messagingSenderId: "867511991001",
-        appId: "1:867511991001:web:bcff279318ec224859dcc8",
-        measurementId: "G-QCED8REGCD"
-});
-
-const messaging = firebase.messaging();
-
-messaging.setBackgroundMessageHandler(function(payload) {
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
-
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
-
-// Sisanya kode Anda...
-
 'use strict';
 const CACHE_VERSION = 1;
 let CURRENT_CACHES = {
