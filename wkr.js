4a15e188592e45f271d7fa299783cbba34466a11
pemersatudotfun <128120945+pemersatudotfun@users.noreply.github.com>
2023-11-20 23:20:02 +0700

    Update wkr.js

--- wkr.js
+++ wkr.js
@@ -80,3 +80,10 @@ self.addEventListener('fetch', event => {
   }
 
 });
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
+});
