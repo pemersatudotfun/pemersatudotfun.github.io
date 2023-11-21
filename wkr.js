9117d0191742fb13324fe4565647fd70bbf61256
pemersatudotfun <128120945+pemersatudotfun@users.noreply.github.com>
2023-11-22 01:27:13 +0700

    Update wkr.js

--- wkr.js
+++ wkr.js
@@ -7,14 +7,10 @@ const OFFLINE_URL = 'detect-movie.html';
 
 function createCacheBustedRequest(url) {
   let request = new Request(url, {cache: 'reload'});
-  // See https://fetch.spec.whatwg.org/#concept-request-mode
-  // This is not yet supported in Chrome as of M48, so we need to explicitly check to see
-  // if the cache: 'reload' option had any effect.
   if ('cache' in request) {
     return request;
   }
 
-  // If {cache: 'reload'} didn't have any effect, append a cache-busting URL parameter instead.
   let bustedUrl = new URL(url, self.location.href);
   bustedUrl.search += (bustedUrl.search ? '&' : '') + 'cachebust=' + Date.now();
   return new Request(bustedUrl);
@@ -22,8 +18,6 @@ function createCacheBustedRequest(url) {
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    // We can't use cache.add() here, since we want OFFLINE_URL to be the cache key, but
-    // the actual URL we end up requesting might include a cache-busting parameter.
     fetch(createCacheBustedRequest(OFFLINE_URL)).then(function(response) {
       return caches.open(CURRENT_CACHES.offline).then(function(cache) {
         return cache.put(OFFLINE_URL, response);
@@ -33,9 +27,6 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  // Delete all caches that aren't named in CURRENT_CACHES.
-  // While there is only one cache in this example, the same logic will handle the case where
-  // there are multiple versioned caches.
   let expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
     return CURRENT_CACHES[key];
   });
@@ -45,9 +36,6 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (expectedCacheNames.indexOf(cacheName) === -1) {
-            // If this cache name isn't present in the array of "expected" cache names,
-            // then delete it.
-            //console.log('Deleting out of date cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -57,33 +45,41 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  // request.mode of 'navigate' is unfortunately not supported in Chrome
-  // versions older than 49, so we need to include a less precise fallback,
-  // which checks for a GET request with an Accept: text/html header.
   if (event.request.mode === 'navigate' ||
       (event.request.method === 'GET' &&
        event.request.headers.get('accept').includes('text/html'))) {
-   // console.log('Handling fetch event for', event.request.url);
     event.respondWith(
       fetch(event.request).catch(error => {
-        // The catch is only triggered if fetch() throws an exception, which will most likely
-        // happen due to the server being unreachable.
-        // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
-        // range, the catch() will NOT be called. If you need custom handling for 4xx or 5xx
-        // errors, see https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
-       // console.log('Fetch failed; returning offline page instead.', error);
         return caches.match(OFFLINE_URL);
       })
     );
   }
-
 });
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
+    var url = event.notification.data.url;
     event.waitUntil(
-        clients.openWindow(event.notification.data.url)
+        clients.matchAll({
+            type: 'window'
+        })
+        .then(function(windowClients) {
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
     );
+});
+
+self.addEventListener('message', function(event) {
+  const { notificationTitle1, notificationOptions1, notificationTitle2, notificationOptions2 } = event.data;
+
+  self.registration.showNotification(notificationTitle1, notificationOptions1);
+  self.registration.showNotification(notificationTitle2, notificationOptions2);
 });
